643beb33c10d8130b75dd84664ed06304e266255
administrator <administrator@cchalifo.xyz>
2022-08-31 07:24:01 -0400

    modified:   s.sh

--- s.sh
+++ s.sh
@@ -1,1 +1,10 @@
-ls -la /
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null 
+
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list 
+apt update
+
+apt install nginx -y
+
